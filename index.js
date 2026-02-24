911b1ff4cd7089232defe5c37907cfcd084266a6
mohitSinghXd <mohitsingh.builds@gmail.com>
2026-02-24 22:51:07 +0530

    fix cors

--- index.js
+++ index.js
@@ -22,12 +22,13 @@ app.post(
   stripeWebhook
 );
 
-app.use(cors(
-    {origin:"http://localhost:5173",
-        credentials:true,
-        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
-    }
-))
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://frontend-n-oti-git-main-mohits-projects-76b2ce06.vercel.app"
+  ],
+  credentials: true
+}))
 
 
 
